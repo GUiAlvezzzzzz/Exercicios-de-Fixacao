0c462081b84f397ee88ce49aba7afa39877e2aa1
GUiAlvezzzzzz <143121343+GUiAlvezzzzzz@users.noreply.github.com>
2023-10-15 22:48:08 -0300

    Commit 02
    a) Crie uma tabela produtos com as colunas produto, preco e quantidade.
    
    b) Utilize a função ROUND() para arredondar os preços para 2 casas decimais.
    
    c) Use a função ABS() para exibir o valor absoluto das quantidades.
    
    d) Calcule a média dos preços dos produtos utilizando a função AVG().

--- exerciciosDeFixacao20231002.sql
+++ exerciciosDeFixacao20231002.sql
@@ -13,3 +13,18 @@ SELECT
         ELSE CONCAT('Sra. ', nome)
     END AS nome_com_tratamento
 FROM nomes;
+
+CREATE TABLE produtos (
+    produto VARCHAR(50),
+    preco DECIMAL(10, 2),
+    quantidade INT
+);
+
+INSERT INTO produtos (produto, preco, quantidade)
+VALUES ('Produto A', 10.99, 5), ('Produto B', 15.50, 0), ('Produto C', 25.75, -2);
+
+SELECT produto, ROUND(preco, 2) AS preco_arredondado FROM produtos;
+
+SELECT produto, ABS(quantidade) AS quantidade_absoluta FROM produtos;
+
+SELECT AVG(preco) AS media_precos FROM produtos;
