98934b9c636de6d5a5d23696fb3bd5fa7bce1edc
GUiAlvezzzzzz <143121343+GUiAlvezzzzzz@users.noreply.github.com>
2023-10-15 22:45:12 -0300

    Commit 01 - A;B;C & D
    a) Crie uma tabela nomes com uma coluna nome contendo os seguintes nomes:
    
    Roberta
    Roberto
    Maria Clara
    João
    b) Utilize a função UPPER() para converter todos os nomes para maiúsculas.
    
    c) Use a função LENGTH() para determinar o tamanho de cada nome.
    
    d) Use a função CONCAT() para adicionar "Sr. " antes dos nomes masculinos e "Sra. " antes dos nomes femininos.

---
+++ exerciciosDeFixacao20231002.sql
@@ -0,0 +1,15 @@
+CREATE TABLE nomes (
+    nome VARCHAR(50)
+);
+
+INSERT INTO nomes (nome)
+VALUES ('Roberta'), ('Roberto'), ('Maria Clara'), ('João');
+
+SELECT UPPER(nome) FROM nomes;
+SELECT nome, LENGTH(nome) AS tamanho FROM nomes;
+SELECT 
+    CASE 
+        WHEN nome LIKE '%João%' OR nome LIKE '%Roberto%' THEN CONCAT('Sr. ', nome)
+        ELSE CONCAT('Sra. ', nome)
+    END AS nome_com_tratamento
+FROM nomes;
